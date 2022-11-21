b8c77c49d1f76ac01be2d34ba29593ddef1bb92e
Nadav Ribak <nribak@shutterfly.com>
2022-11-21 15:05:54 +0200

    files move

--- src/index.tsx
+++ src/index.tsx
@@ -1,9 +1,8 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import 'mdb-react-ui-kit/dist/css/mdb.min.css';
-import './components/styles/app.css';
+import './styles/app.css';
 
-// import "@fortawesome/fontawesome-free/css/all.min.css";
 import App from "./components/App";
 
 const root = ReactDOM.createRoot(
