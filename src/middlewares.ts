95234ca99992abe563f3cd80c3bcfab51e6b86fb
Nadav Ribak <nribak@shutterfly.com>
2022-11-25 15:16:57 +0200

    applied stronger api key

--- src/middlewares.ts
+++ src/middlewares.ts
@@ -1,14 +1,13 @@
 import {NextFunction, Request, Response} from "express";
 import colors from "colors/safe";
 
-const privateKey = 'abcd';
-
 export const loggerMiddleware = ((req: Request, res: Response, next: NextFunction) => {
     console.log(colors.green(req.method) + ':', colors.bgGreen(req.url));
     next();
 });
 
 export const authMiddleware = ((req: Request, res: Response, next: NextFunction) => {
+    const privateKey = process.env.API_KEY;
    const {authorization} = req.headers;
    if(authorization === privateKey)
        next();
