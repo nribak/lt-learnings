ec77f75c4a8fdac60c3d65f105013515ed2ae359
Nadav Ribak <nribak@shutterfly.com>
2022-11-25 14:42:00 +0200

    fixed scripts

--- src/index.ts
+++ src/index.ts
@@ -4,10 +4,8 @@ import mongoose from "mongoose";
 require('dotenv').config()
 
 const mongoUri = `${process.env.ATLAS_ENDPOINT}/${process.env.ATLAS_TABLE}`;
-// const port = parseInt(process.env.EXPRESS_PORT!);
-const port: any = process.env.EXPRESS_PORT || process.env.PORT || 80;
-const host = process.env.YOUR_HOST || `0.0.0.0`;
+const port = process.env.PORT || 4000
 mongoose.connect(mongoUri).then(() => {
     console.log('MongoDB is connected');
 });
-app.listen(port, host,() => console.log(`posts service running: ${port}`));
+app.listen(port,() => console.log(`posts service running: ${port}`));
